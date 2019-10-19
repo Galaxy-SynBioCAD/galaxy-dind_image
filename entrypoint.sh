6e3e3f00dca105c6276616e3c2b7c12faf080c7c
joan <joan.herisson@univ-evry.fr>
2019-10-19 11:30:33 +0200

    Add crontab

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,5 +9,8 @@ echo "`ping -c1 db | sed -nE 's/^PING[^(]+\(([^)]+)\).*/\1/p'` db" >> /etc/hosts
 service docker start
 update-rc.d docker enable
 
+if ! crontab -l | grep -q rest-mon; then
+  (crontab -l 2>/dev/null; echo \"*/5 * * * * bash /galaxy/tools/rest-mon.sh\") | crontab -
+fi
 
 runuser -l galaxy -c 'cd /galaxy ; sh run.sh'
