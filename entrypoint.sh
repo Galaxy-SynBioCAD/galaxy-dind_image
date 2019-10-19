dabf75edbe43ab412e177aaf1f6e426b95ae760a
joan <joan.herisson@univ-evry.fr>
2019-10-19 11:07:22 +0200

    Manage docker service

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,8 @@ rm -f /var/run/docker.pid
 cat /resolv.conf >> /etc/resolv.conf
 echo "`ping -c1 db | sed -nE 's/^PING[^(]+\(([^)]+)\).*/\1/p'` db" >> /etc/hosts
 
-dockerd&
+systemctl start docker
+systemctl enable docker
+
 
 runuser -l galaxy -c 'cd /galaxy ; sh run.sh'
