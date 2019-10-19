65622ff8fab4069178ed3d62f94ecd320cd5ed19
joan <joan.herisson@univ-evry.fr>
2019-10-19 11:37:21 +0200

    Typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,7 +10,7 @@ service docker start
 update-rc.d docker enable
 
 if ! crontab -l | grep -q rest-mon; then
-  (crontab -l 2>/dev/null; echo \"*/5 * * * * bash /galaxy/tools/rest-mon.sh\") | crontab -
+  (crontab -l 2>/dev/null; echo "*/5 * * * * bash /galaxy/tools/rest-mon.sh") | crontab -
 fi
 
 runuser -l galaxy -c 'cd /galaxy ; sh run.sh'
